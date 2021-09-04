a76695100f2134cc77cdfd27d1fcd14971d5e0a5
Noel Chetty <noelchetty@Noel-C-MacBook-Pro.local>
2021-09-04 10:10:51 +0800

    first commit

--- cicd-implemention/src/main/java/com/fsd/phase5/cicdimplemention/CicdImplementionApplication.java
+++ cicd-implemention/src/main/java/com/fsd/phase5/cicdimplemention/CicdImplementionApplication.java
@@ -2,9 +2,17 @@ package com.fsd.phase5.cicdimplemention;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdImplementionApplication {
+	
+	  @RequestMapping("/")
+	  public String home() {
+	    return "CiCd Implementaion with Docker.. works !!!";
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdImplementionApplication.class, args);
