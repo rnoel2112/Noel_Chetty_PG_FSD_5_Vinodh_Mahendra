b9d1c2aeb398a750aac820782c41cb53bad7aecc
Noel Chetty <noelchetty@Noel-C-MacBook-Pro.local>
2021-09-04 10:01:50 +0800

    first commit

---
+++ cicd-implemention/src/main/java/com/fsd/phase5/cicdimplemention/CicdImplementionApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.phase5.cicdimplemention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdImplementionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdImplementionApplication.class, args);
+	}
+
+}
---
+++ cicd-implemention/src/test/java/com/fsd/phase5/cicdimplemention/CicdImplementionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsd.phase5.cicdimplemention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdImplementionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
